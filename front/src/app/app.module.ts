952061283399e91d14ac3e5aacc9bf274e1f6f82
Miguel Vasco Macamo <miguelvascomacamo@gmail.com>
2017-11-10 17:03:45 -0800

    importando observable

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -6,7 +6,7 @@ import { MDBBootstrapModule } from 'angular-bootstrap-md';
 import { AppComponent } from './app.component';
 import { AdminComponent } from './admin/admin.component';
 import { MenuAdminComponent } from './admin/menu-admin/menu-admin.component';
-import 'rxjs/Rx';
+import { Observable } from 'rxjs/Rx';
 
 @NgModule({
 declarations: [
