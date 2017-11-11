27558778a8300ed475ed25874046d61f21a19825
Miguel Vasco Macamo <miguelvascomacamo@gmail.com>
2017-11-11 12:51:47 -0800

    importando esquemas

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -1,27 +1,37 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { AdminComponent } from './admin/admin.component';
-import { MenuAdminComponent } from './admin/menu-admin/menu-admin.component';
-import { PaginaInicialComponent } from './pagina-inicial/pagina-inicial.component';
-import { UsuariosComponent } from './admin/usuarios/usuarios.component';
-import { MenuComponent } from './pagina-inicial/menu/menu.component';
+import {AppComponent} from './app.component';
+import {AdminComponent} from './admin/admin.component';
+import {MenuAdminComponent} from './admin/menu-admin/menu-admin.component';
+import {PaginaInicialComponent} from './pagina-inicial/pagina-inicial.component';
+import {UsuariosComponent} from './admin/usuarios/usuarios.component';
+import {MenuComponent} from './pagina-inicial/menu/menu.component';
+import {FormsModule} from "@angular/forms";
+import {HttpClient} from "@angular/common/http";
+
+import {NO_ERRORS_SCHEMA} from '@angular/core';
+import {MDBBootstrapModule} from 'angular-bootstrap-md';
 
 @NgModule({
-declarations: [
-AppComponent,
-AdminComponent,
-MenuAdminComponent,
-PaginaInicialComponent,
-UsuariosComponent,
-MenuComponent
-],
-imports: [
-BrowserModule
+    declarations: [
+        AppComponent,
+        AdminComponent,
+        MenuAdminComponent,
+        PaginaInicialComponent,
+        UsuariosComponent,
+        MenuComponent
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        HttpClient,
+        MDBBootstrapModule.forRoot()
 
-],
-providers: [],
-bootstrap: [AppComponent]
+    ],
+    schemas: [ NO_ERRORS_SCHEMA ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
