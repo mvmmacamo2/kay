41f20d596d6c2dea8665aa902d367c782f5849d6
Miguel Vasco Macamo <miguelvascomacamo@gmail.com>
2017-11-10 16:18:29 -0800

    importando charts e fontes em modules

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -1,20 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
 import { AppComponent } from './app.component';
 import { AdminComponent } from './admin/admin.component';
 import { MenuAdminComponent } from './admin/menu-admin/menu-admin.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    AdminComponent,
-    MenuAdminComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+declarations: [
+AppComponent,
+AdminComponent,
+MenuAdminComponent
+],
+imports: [
+BrowserModule,
+MDBBootstrapModule.forRoot()
+],
+schemas: [ NO_ERRORS_SCHEMA ],
+providers: [],
+bootstrap: [AppComponent]
 })
 export class AppModule { }
