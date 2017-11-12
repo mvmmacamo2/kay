296990e388778c127a32ad70f5f1d0628c0f1f7b
Miguel Vasco Macamo <miguelvascomacamo@gmail.com>
2017-11-11 20:47:57 -0800

    criando o modulo detalhe-usuario

--- front/src/app/admin/usuarios/usuarios.module.ts
+++ front/src/app/admin/usuarios/usuarios.module.ts
@@ -2,6 +2,7 @@ import {NgModule} from "@angular/core";
 import {CommonModule} from "@angular/common";
 import {UsuariosComponent} from "./usuarios.component";
 import { NovoUsuarioComponent } from './novo-usuario/novo-usuario.component';
+import { DetalheUsuarioComponent } from './detalhe-usuario/detalhe-usuario.component';
 
 
 @NgModule({
@@ -12,7 +13,8 @@ import { NovoUsuarioComponent } from './novo-usuario/novo-usuario.component';
     exports: [],
     declarations: [
         UsuariosComponent,
-        NovoUsuarioComponent],
+        NovoUsuarioComponent,
+        DetalheUsuarioComponent],
     providers:[]
 
 
