246af5dd51140debb93d77eb0ebab126297edaf4
Miguel Vasco Macamo <miguelvascomacamo@gmail.com>
2017-11-11 19:37:18 -0800

    adicionando os imports

--- front/src/app/pagina-inicial/pagina-inicial.module.ts
+++ front/src/app/pagina-inicial/pagina-inicial.module.ts
@@ -1,9 +1,12 @@
-
-
+import {NgModule} from "@angular/core";
 
 @NgModule({
-
+    imports: [],
+    exports: [],
+    declarations: [],
+    providers: []
 
 })
 
-export class  PaginaInicialModule {}
+export class PaginaInicialModule {
+}
